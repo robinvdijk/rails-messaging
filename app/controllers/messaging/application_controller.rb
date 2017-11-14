38a105289379e88cd9959dcacb2b0617d0465660
Robin van Dijk <rvandijk@rovecom.nl>
2017-11-14 16:32:15 +0100

    Changed before_filter to before_action

--- app/controllers/messaging/application_controller.rb
+++ app/controllers/messaging/application_controller.rb
@@ -1,6 +1,6 @@
 module Messaging
   class ApplicationController < ActionController::Base
-    before_filter :authenticate_messaging_user!
+    before_action :authenticate_messaging_user!
 
     def current_user
       current_messaging_user
